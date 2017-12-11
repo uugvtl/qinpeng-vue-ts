6a94ab271e0a81cad7bf3bbbf56402821865f921
Leon <uugv_tl@163.com>
2017-12-11 22:45:07 +0800

    project init finish

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'element-ui/lib/theme-chalk/index.css'
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+
+import VueRx from 'vue-rx'
+import { Observable } from 'rxjs/Observable'
+import { Subscription } from 'rxjs/Subscription' // Disposable if using RxJS4
+import { Subject } from 'rxjs/Subject' // required for domStreams option
+
+// tada!
+Vue.use(VueRx, {
+  Observable,
+  Subscription,
+  Subject
+})
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
