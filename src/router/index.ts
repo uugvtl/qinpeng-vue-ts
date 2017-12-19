08324668fa112e2259bb5268276c11ebbd1f752f
Leon <uugv_tl@163.com>
2017-12-19 16:47:13 +0800

    完成首页的界面

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
-const HelloWorld = () => import('@/components/HelloWorld.vue')
 const Login = () => import('@/containers/Login.vue')
 
 export default new Router({
   routes: [
-    { path: '/', component: HelloWorld },
-    { path: '/login', component: Login }
+    { path: '/', component: Login }
   ]
 })
